9403446fb6466bd9684159803cc6e65dad4df84c
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-14 11:00:18 -0300

    fix linters

--- enumerarable.rb
+++ enumerarable.rb
@@ -1,31 +1,27 @@
 module Myenumerable
-    def any?
+  def any?
       return 'No block supplied' unless block_given?
-  
       each do |item|
         return true if yield item
       end
-  
       false
     end
-  
-    def all?
+
+  def all?
       return 'No block supplied' unless block_given?
-  
+
       each { |item| return false unless yield item }
-  
       true
     end
-  
-    def filter
+
+  def filter
       rt = []
       return rt.to_enum unless block_given?
-  
+
       each do |item|
         rt.push(item) if yield item
       end
-  
+
       rt
     end
-  end
-  
+end
