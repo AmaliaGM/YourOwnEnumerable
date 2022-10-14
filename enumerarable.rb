fe158f6ec6b718aa4729bc6b44b62316877a407f
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-10-14 15:20:57 -0300

    rubocop missbehaving

--- enumerarable.rb
+++ enumerarable.rb
@@ -1,28 +1,28 @@
 module Myenumerable
   def any?
-  return 'No block supplied' unless block_given?
+    return 'No block supplied' unless block_given?
 
-      each do |item|
-        return true if yield item
-      end
-      false
+    each do |item|
+      return true if yield item
+    end
+    false
   end
 
   def all?
-  return 'No block supplied' unless block_given?
+    return 'No block supplied' unless block_given?
 
-      each { |item| return false unless yield item }
-      true
+    each { |item| return false unless yield item }
+    true
   end
 
   def filter
-  rt = []
-      return rt.to_enum unless block_given?
+    rt = []
+    return rt.to_enum unless block_given?
 
-      each do |item|
-        rt.push(item) if yield item
-      end
+    each do |item|
+      rt.push(item) if yield item
+    end
 
-      rt
+    rt
   end
 end
